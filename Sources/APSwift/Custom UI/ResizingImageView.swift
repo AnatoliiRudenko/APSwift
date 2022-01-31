e6c85f045bbea13fecfe5f5aefa5d799ce5c88d6
Anatoly Rudenko <rudenkoap97@gmail.com>
2022-01-31 20:17:01 +0700

    extend ResizingImageView

--- Sources/APSwift/Custom UI/ResizingImageView.swift
+++ Sources/APSwift/Custom UI/ResizingImageView.swift
@@ -9,7 +9,15 @@ import UIKit
 
 open class ResizingImageView: UIImageView {
     
-    var expectedWidth: CGFloat = UIScreen.main.bounds.width
+    var expectedWidth: CGFloat = UIScreen.main.bounds.width {
+        didSet {
+            self.fitsSuperviewWidth = false
+            self.width = expectedWidth
+        }
+    }
+    var fitsSuperviewWidth = true
+    
+    private var width: CGFloat = 0
     
     convenience init() {
         self.init(frame: CGRect())
@@ -17,19 +25,32 @@ open class ResizingImageView: UIImageView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        contentMode = .scaleAspectFit
+        setup()
     }
     
     required public init?(coder: NSCoder) {
         super.init(coder: coder)
+        setup()
+    }
+    
+    private func setup() {
         contentMode = .scaleAspectFit
+    }
+    
+    open override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        if fitsSuperviewWidth {
+            width = superview?.bounds.width ?? 0
+            invalidateIntrinsicContentSize()
+        }
     }
     
     open override var intrinsicContentSize: CGSize {
         guard let myImage = self.image else { return CGSize() }
         let myImageWidth = myImage.size.width
         let myImageHeight = myImage.size.height
-        let myViewWidth = expectedWidth
+        let myViewWidth = width
         let ratio = myViewWidth / myImageWidth
         let scaledHeight = myImageHeight * ratio
         return CGSize(width: myViewWidth, height: scaledHeight)
