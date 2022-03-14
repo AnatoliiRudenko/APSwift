f511b682e0f451a06dd209325137467e5d4f0c3a
Anatoly Rudenko <rudenkoap97@gmail.com>
2022-03-14 19:13:36 +0700

    extend UIEdgeInsets

--- Sources/APSwift/Extensions/UIEdgeInsets.swift
+++ Sources/APSwift/Extensions/UIEdgeInsets.swift
@@ -24,4 +24,20 @@ extension UIEdgeInsets {
     static func vertical(_ value: CGFloat) -> UIEdgeInsets {
         .init(top: value, left: 0, bottom: value, right: 0)
     }
+    
+    static func top(_ value: CGFloat) -> UIEdgeInsets {
+        .init(top: value, left: 0, bottom: 0, right: 0)
+    }
+    
+    static func bottom(_ value: CGFloat) -> UIEdgeInsets {
+        .init(top: 0, left: 0, bottom: value, right: 0)
+    }
+    
+    static func left(_ value: CGFloat) -> UIEdgeInsets {
+        .init(top: 0, left: value, bottom: 0, right: 0)
+    }
+    
+    static func right(_ value: CGFloat) -> UIEdgeInsets {
+        .init(top: 0, left: 0, bottom: 0, right: value)
+    }
 }
