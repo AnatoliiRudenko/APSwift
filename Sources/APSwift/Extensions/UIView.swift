cb73bec6dc5215109a06bd2391b608605da431fe
Anatoly Rudenko <rudenkoap97@gmail.com>
2022-03-14 15:50:36 +0700

    hotfix

--- Sources/APSwift/Extensions/UIView.swift
+++ Sources/APSwift/Extensions/UIView.swift
@@ -104,9 +104,10 @@ public extension UIView {
         layer.cornerRadius = value
     }
     
-    func roundCorners(corners: [UIRectCorner], radius: CGFloat) {
+    func roundCorners(corners: UIRectCorner, radius: CGFloat) {
         let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
         let mask = CAShapeLayer()
+        mask.frame = frame
         mask.path = path.cgPath
         layer.mask = mask
     }
