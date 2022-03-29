d6128e0de64e28028e79b67825afd7defa2829b2
Anatoly Rudenko <rudenkoap97@gmail.com>
2022-03-29 21:12:12 +0700

    fix

--- Sources/APSwift/Base/BaseButton.swift
+++ Sources/APSwift/Base/BaseButton.swift
@@ -107,9 +107,9 @@ private extension BaseButton {
                                 bottom: titleEdgeInsets.bottom,
                                 right: left ? titleEdgeInsets.right : inset)
         contentEdgeInsets = .init(top: contentEdgeInsets.top,
-                                  left: left ? inset : contentEdgeInsets.left,
+                                  left: left ? inset + contentEdgeInsets.left : contentEdgeInsets.left,
                                   bottom: contentEdgeInsets.bottom,
-                                  right: left ? contentEdgeInsets.right : inset)
+                                  right: left ? contentEdgeInsets.right : inset + contentEdgeInsets.right)
     }
     
     func adjustToSettingImage() {
