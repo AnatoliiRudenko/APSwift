b58791593561df24e2bae4b8aa55f58a7db49c11
Anatoly Rudenko <rudenkoap97@gmail.com>
2022-03-29 20:15:21 +0700

    fix

--- Sources/APSwift/Base/BaseButton.swift
+++ Sources/APSwift/Base/BaseButton.swift
@@ -52,8 +52,8 @@ open class BaseButton: UIButton {
     // MARK: - Resize to fit title label
     open override var intrinsicContentSize: CGSize {
         let labelSize = titleLabel?.sizeThatFits(CGSize(width: titleLabel?.frame.size.width ?? 0, height: .greatestFiniteMagnitude)) ?? .zero
-        let width: CGFloat = labelSize.width + titleEdgeInsets.left + titleEdgeInsets.right + contentEdgeInsets.left + contentEdgeInsets.right + imagesRelatedInsets.left + imagesRelatedInsets.right
-        let height: CGFloat = labelSize.height + titleEdgeInsets.top + titleEdgeInsets.bottom + contentEdgeInsets.top + contentEdgeInsets.bottom + imagesRelatedInsets.top + imagesRelatedInsets.bottom
+        let width: CGFloat = labelSize.width + titleEdgeInsets.left + titleEdgeInsets.right + contentEdgeInsets.left + contentEdgeInsets.right
+        let height: CGFloat = labelSize.height + titleEdgeInsets.top + titleEdgeInsets.bottom + contentEdgeInsets.top + contentEdgeInsets.bottom
         return CGSize(width: width, height: height)
     }
     
