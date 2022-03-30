09de0376ba9e3286b4f975cb1fd873c8fabca451
Anatoly Rudenko <rudenkoap97@gmail.com>
2022-03-30 14:15:39 +0700

    fix swipes

--- Sources/APSwift/Services/Swipe.swift
+++ Sources/APSwift/Services/Swipe.swift
@@ -8,10 +8,26 @@
 import UIKit
 
 public struct Swipe {
-    public var onLeft: DataClosure<UISwipeGestureRecognizer>?
-    public var onRight: DataClosure<UISwipeGestureRecognizer>?
-    public var onDown: DataClosure<UISwipeGestureRecognizer>?
-    public var onUp: DataClosure<UISwipeGestureRecognizer>?
+    public var onLeft: DataClosure<UISwipeGestureRecognizer>? {
+        didSet {
+            didSetDirection?(.left)
+        }
+    }
+    public var onRight: DataClosure<UISwipeGestureRecognizer>? {
+        didSet {
+            didSetDirection?(.right)
+        }
+    }
+    public var onDown: DataClosure<UISwipeGestureRecognizer>? {
+        didSet {
+            didSetDirection?(.down)
+        }
+    }
+    public var onUp: DataClosure<UISwipeGestureRecognizer>? {
+        didSet {
+            didSetDirection?(.up)
+        }
+    }
     
     internal var didSetDirection: DataClosure<UISwipeGestureRecognizer.Direction>?
 }
