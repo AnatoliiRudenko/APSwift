c03c5b7f9424bb6eee80a105ceec7c658ac8cc1b
Anatoly Rudenko <rudenkoap97@gmail.com>
2022-03-31 08:00:03 +0700

    fix GradientView

--- Sources/APSwift/Custom UI/GradientView.swift
+++ Sources/APSwift/Custom UI/GradientView.swift
@@ -7,43 +7,45 @@
 
 import UIKit
 
-class GradientView: BaseView {
+open class GradientView: BaseView {
     
-    var color = UIColor.white {
+    public var colors: [UIColor]? {
+        didSet {
+            gradient.colors = colors
+        }
+    }
+    public var color: UIColor? {
         didSet {
             gradient.colors = [color.withAlphaComponent(0).cgColor, color.cgColor]
         }
     }
-    var endPoint = CGPoint(x: 0, y: 0) {
+    public var endPoint = CGPoint(x: 0, y: 0) {
         didSet {
             gradient.endPoint = endPoint
         }
     }
-    var startPoint = CGPoint(x: 0, y: 1) {
+    public var startPoint = CGPoint(x: 0, y: 1) {
         didSet {
             gradient.startPoint = startPoint
         }
     }
     
     // MARK: - Life cycle
-    override func setupComponents() {
+    open override func setupComponents() {
         super.setupComponents()
         
         clipsToBounds = true
-        
-        gradient.colors = [color.withAlphaComponent(0).cgColor, color.cgColor]
         layer.addSublayer(gradient)
-        
         gradient.endPoint = endPoint
         gradient.startPoint = startPoint
     }
     
-    override func layoutSublayers(of layer: CALayer) {
+    open override func layoutSublayers(of layer: CALayer) {
         super.layoutSublayers(of: layer)
         
         gradient.frame = bounds
     }
     
     // MARK: - UI Properties
-    private let gradient = CAGradientLayer()
+    public let gradient = CAGradientLayer()
 }
