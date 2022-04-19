d75854b65b3e8c769850d94074f7cf61bbf99d17
Anatoly Rudenko <rudenkoap97@gmail.com>
2022-04-19 20:13:31 +0400

    extend conteiner view

--- Sources/APSwift/Custom UI/ContainerView.swift
+++ Sources/APSwift/Custom UI/ContainerView.swift
@@ -11,16 +11,12 @@ open class ContainerView<Content: UIView>: BaseView {
 
     private(set) var content: Content
     private var insets: UIEdgeInsets = .zero
+    private var minInsets: UIEdgeInsets?
     
     open override func setupComponents() {
         super.setupComponents()
         addSubview(content)
-        content.snp.makeConstraints { make in
-            make.top.equalToSuperview().inset(insets.top)
-            make.bottom.equalToSuperview().inset(insets.bottom)
-            make.left.equalToSuperview().inset(insets.left)
-            make.right.equalToSuperview().inset(insets.right)
-        }
+        minInsets == nil ? addEqualToConstraints() : addGreaterThatOrEqualToConstraints()
     }
 
     public init(content: Content, insets: UIEdgeInsets = .zero) {
@@ -29,7 +25,35 @@ open class ContainerView<Content: UIView>: BaseView {
         super.init(frame: .zero)
     }
     
+    public init(content: Content, minInsets: UIEdgeInsets = .zero) {
+        self.content = content
+        self.minInsets = minInsets
+        super.init(frame: .zero)
+    }
+    
     required public init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension ContainerView {
+    
+    func addEqualToConstraints() {
+        content.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(insets.top)
+            make.bottom.equalToSuperview().inset(insets.bottom)
+            make.left.equalToSuperview().inset(insets.left)
+            make.right.equalToSuperview().inset(insets.right)
+        }
+    }
+    
+    func addGreaterThatOrEqualToConstraints() {
+        content.snp.makeConstraints { make in
+            make.top.greaterThanOrEqualToSuperview().inset(insets.top)
+            make.bottom.greaterThanOrEqualToSuperview().inset(insets.bottom)
+            make.left.greaterThanOrEqualToSuperview().inset(insets.left)
+            make.right.greaterThanOrEqualToSuperview().inset(insets.right)
+            make.center.equalToSuperview()
+        }
     }
 }
