54930fb1d7c0612e946435f7c4f46389caf2bc74
Anatoly Rudenko <rudenkoap97@gmail.com>
2022-04-25 22:50:58 +0400

    extend NSDirectionalEdgeInsets

---
+++ Sources/APSwift/Extensions/NSDirectionalEdgeInsets.swift
@@ -0,0 +1,43 @@
+//
+//  File.swift
+//  
+//
+//  Created by Анатолий Руденко on 25.04.2022.
+//
+
+import UIKit
+
+public extension NSDirectionalEdgeInsets {
+    
+    static func allAround(_ value: CGFloat) -> NSDirectionalEdgeInsets {
+        .init(top: value, leading: value, bottom: value, trailing: value)
+    }
+    
+    static func directional(vertical: CGFloat, horizontal: CGFloat) -> NSDirectionalEdgeInsets {
+        .init(top: vertical, leading: horizontal, bottom: vertical, trailing: horizontal)
+    }
+    
+    static func horizontal(_ value: CGFloat) -> NSDirectionalEdgeInsets {
+        .init(top: 0, leading: value, bottom: 0, trailing: value)
+    }
+    
+    static func vertical(_ value: CGFloat) -> NSDirectionalEdgeInsets {
+        .init(top: value, leading: 0, bottom: value, trailing: 0)
+    }
+    
+    static func top(_ value: CGFloat) -> NSDirectionalEdgeInsets {
+        .init(top: value, leading: 0, bottom: 0, trailing: 0)
+    }
+    
+    static func bottom(_ value: CGFloat) -> NSDirectionalEdgeInsets {
+        .init(top: 0, leading: 0, bottom: value, trailing: 0)
+    }
+    
+    static func left(_ value: CGFloat) -> NSDirectionalEdgeInsets {
+        .init(top: 0, leading: value, bottom: 0, trailing: 0)
+    }
+    
+    static func right(_ value: CGFloat) -> NSDirectionalEdgeInsets {
+        .init(top: 0, leading: 0, bottom: 0, trailing: value)
+    }
+}
