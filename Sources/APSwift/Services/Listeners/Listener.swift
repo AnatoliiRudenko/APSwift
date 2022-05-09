d8dfc2e1e8f65eea2927a11b5dce1ef7b876b188
Anatoly Rudenko <rudenkoap97@gmail.com>
2022-05-09 18:06:18 +0400

    + Listeners

---
+++ Sources/APSwift/Services/Listeners/Listener.swift
@@ -0,0 +1,10 @@
+//
+//  File.swift
+//  
+//
+//  Created by Анатолий Руденко on 09.05.2022.
+//
+
+import Foundation
+
+open class Listener: NSObject { }
---
+++ Sources/APSwift/Services/Listeners/Listeners.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//  
+//
+//  Created by Анатолий Руденко on 09.05.2022.
+//
+
+import Foundation
+
+open class Listeners<listener: Listener> {
+    
+    public private(set) var listeners: [Listener] = []
+    
+    public var current: Listener? {
+        self.listeners.last
+    }
+    
+    open func appendListener(_ listener: Listener) {
+        self.removeListener(listener)
+        self.listeners += [listener]
+    }
+
+    open func removeListener(_ listener: Listener) {
+        self.listeners.removeAll(where: { $0 == listener })
+    }
+}
