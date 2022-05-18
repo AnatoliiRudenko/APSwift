c1d7e479656bfc510cccf0b9c92e5fb6a147c291
Anatoly Rudenko <rudenkoap97@gmail.com>
2022-05-18 20:10:15 +0400

    extend Date

--- Sources/APSwift/Extensions/Date.swift
+++ Sources/APSwift/Extensions/Date.swift
@@ -9,10 +9,20 @@ import Foundation
 
 public extension Date {
     
-    func outputString(date: Date?, format: String, timeZone: TimeZone? = nil, locale: Locale = .current) -> String? {
+    func outputString(date: Date?,
+                      format: String,
+                      timeZone: TimeZone? = nil,
+                      locale: Locale = .current,
+                      hasDaySuffix: Bool = false) -> String? {
         guard let date = date else { return nil }
+        var outputFormat = format
+        if hasDaySuffix,
+           let dayLastIndex = format.lastIndex(of: "d") {
+            let nextIndex = format.index(after: dayLastIndex)
+            outputFormat.insert(contentsOf: "'\(daySuffix(day: date.get(.day)))'", at: nextIndex)
+        }
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = format
+        dateFormatter.dateFormat = outputFormat
         dateFormatter.timeZone = timeZone
         dateFormatter.locale = locale
         return dateFormatter.string(from: date)
@@ -29,5 +39,16 @@ public extension Date {
         default:
             return "th"
         }
+    }
+}
+
+public extension Date {
+    
+    func get(_ components: Calendar.Component..., calendar: Calendar = Calendar.current) -> DateComponents {
+        return calendar.dateComponents(Set(components), from: self)
+    }
+
+    func get(_ component: Calendar.Component, calendar: Calendar = Calendar.current) -> Int {
+        return calendar.component(component, from: self)
     }
 }
