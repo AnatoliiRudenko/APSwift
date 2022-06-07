45622845be8d883762e71fdec48c48e0ca30e444
Anatoly Rudenko <rudenkoap97@gmail.com>
2022-06-07 23:13:35 +0400

    + BaseSegmentedControl

---
+++ Sources/APSwift/Base/BaseSegmentedControl.swift
@@ -0,0 +1,46 @@
+//
+//  File.swift
+//  
+//
+//  Created by Анатолий Руденко on 07.06.2022.
+//
+
+import UIKit
+
+class BaseSegmentedControl: UISegmentedControl {
+    
+    // MARK: - Init
+    public convenience init() {
+        self.init(frame: .zero)
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setupComponents()
+    }
+    
+    required public init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupComponents()
+    }
+    
+    open func setupComponents() {
+        
+    }
+    
+    // MARK: - Height Constraint
+    public var height: CGFloat? {
+        didSet {
+            guard let value = self.height else {
+                self.heightConstraint.isActive = false
+                return
+            }
+            self.heightConstraint.constant = value
+            self.heightConstraint.isActive = true
+        }
+    }
+    
+    private lazy var heightConstraint: NSLayoutConstraint = {
+        self.heightAnchor.constraint(equalToConstant: self.height ?? 0)
+    }()
+}
