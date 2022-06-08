e99440f3f81fbedb91ec2ba9e20851dd9f90cb0b
Anatoly Rudenko <rudenkoap97@gmail.com>
2022-06-08 16:36:42 +0400

    extend BaseSegmentedControl

--- Sources/APSwift/Base/BaseSegmentedControl.swift
+++ Sources/APSwift/Base/BaseSegmentedControl.swift
@@ -9,6 +9,48 @@ import UIKit
 
 open class BaseSegmentedControl: UISegmentedControl {
     
+    // MARK: - Props
+    public var didSwitchTo: DataClosure<Int>?
+    
+    // to higlight selected item border
+    public var selectedBorderColor: UIColor?
+    public var normalBorderColor: UIColor?
+    public var cornerRadius: CGFloat?
+    
+    public var selectedTitleColor: UIColor? {
+        didSet {
+            setTitleTextAttributes([.foregroundColor: selectedTitleColor as Any], for: .selected)
+        }
+    }
+    public var normalTitleColor: UIColor? {
+        didSet {
+            setTitleTextAttributes([.foregroundColor: normalTitleColor as Any], for: .normal)
+        }
+    }
+    public var normalBGColor: UIColor? {
+        didSet {
+            setBackgroundImage(normalBGColor?.asImage(size: .init(width: 1, height: height ?? 1)), for: .normal, barMetrics: .default)
+        }
+    }
+    public var selectedBGColor: UIColor? {
+        didSet {
+            setBackgroundImage(selectedBGColor?.asImage(size: .init(width: 1, height: height ?? 1)), for: .selected, barMetrics: .default)
+        }
+    }
+    public var dividerColor: UIColor? {
+        didSet {
+            setDividerImage(dividerColor?.asImage(size: .init(width: 1, height: height ?? 1)), forLeftSegmentState: .normal, rightSegmentState: .normal, barMetrics: .default)
+        }
+    }
+    
+    override open var selectedSegmentIndex: Int {
+        get { super.selectedSegmentIndex }
+        set {
+            super.selectedSegmentIndex = newValue
+            changeUIOnSwitch(newValue, animated: false)
+        }
+    }
+    
     // MARK: - Init
     public override init(items: [Any]?) {
         super.init(items: items)
@@ -26,7 +68,18 @@ open class BaseSegmentedControl: UISegmentedControl {
     }
     
     open func setupComponents() {
+        addTarget(self, action: #selector(didSwitch), for: .valueChanged)
+    }
+    
+    // MARK: - Methods
+    open override func layoutSubviews() {
+        super.layoutSubviews()
         
+        changeUIOnSwitch(selectedSegmentIndex, animated: false)
+    }
+    
+    open func changeUIOnSwitch(_ index: Int, animated: Bool = true) {
+        setSelectedSegmentBorder(animated: animated)
     }
     
     // MARK: - Height Constraint
@@ -44,4 +97,40 @@ open class BaseSegmentedControl: UISegmentedControl {
     private lazy var heightConstraint: NSLayoutConstraint = {
         self.heightAnchor.constraint(equalToConstant: self.height ?? 0)
     }()
+}
+
+// MARK: - Supporting Methods
+private extension BaseSegmentedControl {
+    
+    @objc
+    func didSwitch(_ sender: UISegmentedControl) {
+        didSwitchTo?(sender.selectedSegmentIndex)
+        changeUIOnSwitch(sender.selectedSegmentIndex)
+    }
+    
+    func setSelectedSegmentBorder(animated: Bool = true) {
+        guard let selectedBorderColor = selectedBorderColor,
+              let normalBorderColor = normalBorderColor,
+              let cornerRadius = cornerRadius
+        else { return }
+        let block = { [weak self] in
+            guard let self = self else { return }
+            guard self.subviews.count > self.numberOfSegments else { return }
+            let views = self.subviews.cutToFirst(Int(Double(self.subviews.count) * 0.5))
+            guard !views.isEmpty, (0..<views.count).contains(self.selectedSegmentIndex) else { return }
+            self.clipsToBounds = false
+            views.forEach {
+                $0.addBorder(color: normalBorderColor)
+                $0.roundCorners(cornerRadius)
+            }
+            views[self.selectedSegmentIndex].addBorder(color: selectedBorderColor)
+        }
+        if animated {
+            UIView.animate {
+                block()
+            }
+        } else {
+            block()
+        }
+    }
 }
