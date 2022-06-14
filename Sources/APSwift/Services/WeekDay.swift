de94e1b4b8f368ff05e4a8f0883d200e92567585
Anatoly Rudenko <rudenkoap97@gmail.com>
2022-06-14 14:18:31 +0400

    + WeekDay enum

---
+++ Sources/APSwift/Services/WeekDay.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Анатолий Руденко on 14.06.2022.
+//
+
+import Foundation
+
+public enum WeekDay: Int, CaseIterable {
+    case monday
+    case tueday
+    case wednesday
+    case thursday
+    case friday
+    case saturday
+    case sunday
+}
