b5c074a6993cba223a79dcab98e01c67cfc6adc7
Anatoly Rudenko <rudenkoap97@gmail.com>
2022-06-16 19:11:16 +0400

    + CircleDiagramView

---
+++ Sources/APSwift/Custom UI/CircleDiagramView.swift
@@ -0,0 +1,79 @@
+//
+//  File.swift
+//  
+//
+//  Created by Анатолий Руденко on 16.06.2022.
+//
+
+import Foundation
+
+open class CircleDiagramView: AppView {
+    
+    // MARK: - Props
+    let progressLayer = CAShapeLayer()
+    
+    var diameter: CGFloat = 160
+    var maxValue: CGFloat = 100
+    var minValue: CGFloat = 0
+    var value: CGFloat {
+        currentValue
+    }
+    private var currentValue: CGFloat = 0
+    
+    // MARK: - Methods
+    override func setupComponents() {
+        super.setupComponents()
+     
+        setProgressUI()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        self.draw(value: value, animated: false)
+    }
+    
+    func setValue(_ value: CGFloat, animated: Bool = true) {
+        self.currentValue = value
+        self.draw(value: value, animated: animated)
+    }
+}
+
+// MARK: - Supporting Methods
+private extension CircleDiagramView {
+    
+    func draw(value: CGFloat, animated: Bool) {
+        roundCorners(diameter * 0.5)
+        let startRadians: CGFloat = -.pi / 2 + minValue.asRadians
+        let relativeValue = value / maxValue
+        let targetRadians = relativeValue.asRadians - startRadians
+        let circularPath = UIBezierPath(arcCenter: CGPoint(x: diameter * 0.5, y: diameter * 0.5),
+                                        radius: diameter * 0.5,
+                                        startAngle: startRadians,
+                                        endAngle: targetRadians,
+                                        clockwise: true)
+        progressLayer.path = circularPath.cgPath
+        
+        let progressAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        progressAnimation.toValue = 1
+        progressAnimation.fillMode = .forwards
+        progressAnimation.isRemovedOnCompletion = false
+        progressLayer.add(progressAnimation, forKey: "progressAnimation")
+        
+        guard !animated else { return }
+        CATransaction.begin()
+        CATransaction.setDisableActions(true)
+        progressLayer.strokeEnd = 1
+        CATransaction.commit()
+    }
+    
+    func setProgressUI() {
+        progressLayer.fillColor = UIColor.clear.cgColor
+        progressLayer.strokeColor = UIColor.blue.cgColor
+        progressLayer.lineCap = .round
+        progressLayer.lineWidth = 4
+        progressLayer.strokeEnd = 0
+        guard progressLayer.superlayer == nil else { return }
+        layer.addSublayer(progressLayer)
+    }
+}
---
+++ Sources/APSwift/Extensions/CGFloat.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Анатолий Руденко on 16.06.2022.
+//
+
+import Foundation
+
+public extension CGFloat {
+    
+    var asRadians: CGFloat {
+        self * .pi / 180
+    }
+}
