bc6fc8a5eab246bc9d4c860383df08f4bdf7df7b
Anatoly Rudenko <rudenkoap97@gmail.com>
2022-07-26 20:48:05 +0400

    fix

--- Sources/APSwift/Custom UI/ContainerView.swift
+++ Sources/APSwift/Custom UI/ContainerView.swift
@@ -50,10 +50,10 @@ private extension ContainerView {
     func addGreaterThanOrEqualToConstraints() {
         guard let insets = minInsets else { return }
         content.snp.makeConstraints { make in
-            make.top.greaterThanOrEqualToSuperview().inset(insets.top)
-            make.bottom.greaterThanOrEqualToSuperview().inset(insets.bottom)
-            make.left.greaterThanOrEqualToSuperview().inset(insets.left)
-            make.right.greaterThanOrEqualToSuperview().inset(insets.right)
+            make.top.lessThanOrEqualToSuperview().inset(insets.top)
+            make.bottom.lessThanOrEqualToSuperview().inset(insets.bottom)
+            make.left.lessThanOrEqualToSuperview().inset(insets.left)
+            make.right.lessThanOrEqualToSuperview().inset(insets.right)
             make.center.equalToSuperview()
         }
     }
