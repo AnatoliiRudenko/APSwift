7af7f47ed2c186690915215cbad8205a1daa639e
Anatoly Rudenko <rudenkoap97@gmail.com>
2023-02-03 19:19:16 +0100

    fix

--- Sources/APSwift/Extensions/Encodable.swift
+++ Sources/APSwift/Extensions/Encodable.swift
@@ -14,4 +14,21 @@ public extension Encodable {
         guard let data = try? encoder.encode(self) else { return [:] }
         return (try? JSONSerialization.jsonObject(with: data, options: .allowFragments)).flatMap { $0 as? [String: Any] } ?? [:]
     }
+    
+    var jsonCompatibleData: Any? {
+        guard let array = self as? Array<Encodable> else {
+            return dictionary
+        }
+        let value = array.map { $0.dictionary }
+        return value
+    }
+}
+
+private extension Encodable {
+    
+    var jsonArrayData: Any? {
+        guard let array = self as? Array<Encodable> else { return nil }
+        let value = array.map { $0.dictionary }
+        return value
+    }
 }
