47be313abcc1790c85fdd68d277321d234808093
Anatoly Rudenko <rudenkoap97@gmail.com>
2023-02-28 17:30:06 +0100

    fix

--- Sources/APSwift/Base/BaseTextView.swift
+++ Sources/APSwift/Base/BaseTextView.swift
@@ -62,9 +62,9 @@ open class BaseTextView: UITextView {
         super.layoutSubviews()
 
         guard centersTextVertically else { return }
-        let rect = layoutManager.usedRect(for: textContainer)
-        let topInset = (bounds.size.height - rect.height) * 0.5
-        textContainerInset.top = max(0, topInset)
+        var topCorrect = (self.bounds.size.height - self.contentSize.height * self.zoomScale) * 0.5
+        topCorrect = topCorrect < 0.0 ? 0.0 : topCorrect
+        self.contentInset.top = topCorrect
     }
     
     // MARK: - Height Constraint
