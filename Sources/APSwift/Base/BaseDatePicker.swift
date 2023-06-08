c55a291e91d0709a49dacbade5e2f3662440930c
Anatoly Rudenko <rudenkoap97@gmail.com>
2023-06-08 19:00:22 +0200

    + BaseDatePicker

---
+++ Sources/APSwift/Base/BaseDatePicker.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  
+//
+//  Created by Anatolii Rudenko on 08.06.2023.
+//
+
+import UIKit
+
+open class ViveDatePicker: UIDatePicker {
+    
+    public var didSelectDate: DataClosure<Date>?
+    
+    public init() {
+        super.init(frame: .zero)
+        addTarget(self, action: #selector(datePickerValueChanged), for: .valueChanged)
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc
+    open func datePickerValueChanged(_ sender: UIDatePicker) {
+        self.didSelectDate?(sender.date)
+    }
+}
