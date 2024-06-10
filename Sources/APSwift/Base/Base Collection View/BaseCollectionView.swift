26b07eb0010a24f3c53463af7348690050488726
Anatoly Rudenko <rudenkoap97@gmail.com>
2024-06-10 15:31:47 +0200

    improve offset calculation

--- Sources/APSwift/Base/Base Collection View/BaseCollectionView.swift
+++ Sources/APSwift/Base/Base Collection View/BaseCollectionView.swift
@@ -130,7 +130,7 @@ open class BaseCollectionView<Cell: UICollectionViewCell, Data>: UICollectionVie
     open func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         if alignsSingleItemLeft, collectionView.numberOfItems(inSection: section) == 1 {
             let flowLayout = collectionViewLayout as! UICollectionViewFlowLayout
-            return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: collectionView.frame.width - flowLayout.itemSize.width)
+            return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: collectionView.frame.width - cellSize.width)
         }
         return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
     }
